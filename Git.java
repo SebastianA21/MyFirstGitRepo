db8173afc8704a807d09af089c094cd883565164
SebastianA21 <sebastian.arriaga@uabc.edu.mx>
2023-03-27 17:44:20 -0700

    This is my project

---
+++ Git.java
@@ -0,0 +1,6 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git");
+        System.out.println("This is a Project for my Repository");
+    }
+}
